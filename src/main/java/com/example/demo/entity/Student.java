22aa2ccae4970d80cbe96988aed08b387e9ef079
bala-amypo <bala@amypo.in>
2025-12-17 08:57:21 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -43,7 +43,12 @@ public class Student {
     public void setCgpa(float cgpa) {
         this.cgpa = cgpa;
     }
-    public Student(long) {
+    public Student(Long id, String name, String email, LocalDate dob, float cgpa) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.cgpa = cgpa;
     }
     
 }
--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -5,7 +5,6 @@ import com.example.demo.entity.Student;
 
 public interface StudentService{
     Student insertStudent(Student st);
-    @Service
     List<Student> getAllStudents();
     Optional<Student> getOneStudent(Long id);
     void deleteStudent(Long id);
